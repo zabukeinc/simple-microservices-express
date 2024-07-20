67b92d3c2733551cc2412f98467e98a0a8fb4899
zabukeinc <miftahregyana@gmail.com>
2024-07-20 11:33:17 +0700

    feat(transaction): register config and misc to express

--- services/transaction/index.js
+++ services/transaction/index.js
@@ -1,12 +1,44 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.APP_PORT
+require("dotenv").config();
+require("module-alias/register");
+const express = require("express");
+const sequelize = require("@database/database");
+const app = express();
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Transaction')
-})
+const transactionRoute = require("@transaction-module/infrastructures/routes/transaction.route");
+const MaterialConsumer = require("@transaction-module/infrastructures/brokers/material.consumer");
+const UserConsumer = require("@transaction-module/infrastructures/brokers/user.consumer");
+app.use(express.json());
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`Transaction app listening on port ${port}`)
-})
+// Database
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected.");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+})();
+
+// Consumer
+new MaterialConsumer().run();
+new UserConsumer().run();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World transaction!");
+});
+
+app.use("/transactions", transactionRoute);
+
+const PORT = process.env.APP_PORT || 3000;
+const server = app.listen(PORT, () =>
+  console.log(`transaction Server running on port ${PORT}`),
+);
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+module.exports = app;
