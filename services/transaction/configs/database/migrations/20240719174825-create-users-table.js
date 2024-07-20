32e864bf85558e2633d448897d84f416d5123534
zabukeinc <miftahregyana@gmail.com>
2024-07-20 11:31:13 +0700

    feat(transaction): add migration schema

---
+++ services/transaction/configs/database/migrations/20240719174825-create-users-table.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM("customer", "vendor"),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("users");
+  },
+};
